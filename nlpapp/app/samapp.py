21d26ca0629c3bd1656b0b83203df766f3c44242
Avrpatel <avinash2772@gmail.com>
2022-08-04 12:49:03 -0400

    finished streamlit app

--- nlpapp/app/samapp.py
+++ nlpapp/app/samapp.py
@@ -32,6 +32,57 @@ def set_bg(png_file):
 
 set_bg('../assets/background.png')
 
+######################################################################################
+
+# Functions
+
+# Combining Dataframes
+
+def combining_dfs(df1, df2, df3):
+    all_df=pd.concat([df1['Close'], df2['News_sent'], df3['Alpha_sent']], axis=1, keys=['Close', 'News_sent', 'Alpha_sent'])
+    all_df = all_df.reset_index()
+    return all_df
+
+
+# Plotting combined dataframe
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def create_all_plot(combined_df):
+    # Create figure with secondary y-axis
+    fig_all = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Add traces
+    fig_all.add_trace(
+        go.Scatter(x=combined_df['Date'], y=combined_df['Close'], name="Crypto Price"),
+        secondary_y=False,
+    )
+
+    fig_all.add_trace(
+        go.Scatter(x=combined_df['Date'], y=combined_df['News_sent'], name="News API Score"),
+        secondary_y=True,
+    )
+
+    fig_all.add_trace(
+        go.Scatter(x=combined_df['Date'], y=combined_df['Alpha_sent'], name="Alpha Vantage Score"),
+        secondary_y=True,
+    )
+
+
+    # Add figure title
+    fig_all.update_layout(
+        title_text=f"{option} Price vs. NLP Scores"
+    )
+
+    # Set x-axis title
+    fig_all.update_xaxes(title_text="Date")
+
+    # Set y-axes titles
+    fig_all.update_yaxes(title_text="Crypto Price", secondary_y=False)
+    fig_all.update_yaxes(title_text="Sentiment Scores", secondary_y=True)
+
+    return fig_all
 
 ######################################################################################
 
@@ -124,8 +175,7 @@ elif highlights == 'None':
 
 #########################################################
 
-
-# Radio Button
+# Storing dataframes as specific variable name 
 
 price_df = pd.DataFrame(x_data)
 new_price_df= price_df.reset_index()
@@ -138,6 +188,8 @@ alpha_sentiment_df = pd.DataFrame(z_data)
 final_alpha_df = alpha_sentiment_df.reset_index()
 
 
+# Radio Button to display plots
+
 plot_type = st.radio(
      "Which data set would you like to see a plot for?:",
      ('Crypto Price', 'AlphaVantage', 'NewsAPI', 'All'))
@@ -146,14 +198,14 @@ plot_type = st.radio(
 if plot_type == 'Crypto Price':
      st.write('You selected crypto prices.')
 
-     fig = px.line(new_price_df, x="Date", y="Close", title='Crypto Closing Price')
+     fig = px.line(new_price_df, x="Date", y="Close", title=f'{option} Closing Price')
      st.plotly_chart(fig, use_container_width=True)
      
 
 elif plot_type == 'AlphaVantage':
     st.write("You selected AlphaVantage.")
 
-    fig_alpha_sentiment = px.line(final_alpha_df, x="Date", y="overall_sentiment_score", title='Crypto Closing Price')
+    fig_alpha_sentiment = px.line(final_alpha_df, x="Date", y="Alpha_sent", title=f'{option} Average Sentiment Score - AlphaVantage')
     st.plotly_chart(fig_alpha_sentiment, use_container_width=True)
     
 
@@ -161,7 +213,58 @@ elif plot_type == 'NewsAPI':
     st.write('You selected NewsAPI.')
     for score in crypto_list:
         if score == option:
-            fig_news_sentiment = px.line(final_news_df, x="Date", y=f"{score}_avg", title='Crypto Average Sentiment Score - NewsAPI', markers=True)
+            fig_news_sentiment = px.line(final_news_df, x="Date", y="News_sent", title=f'{option} Average Sentiment Score - NewsAPI', markers=True)
             st.plotly_chart(fig_news_sentiment, use_container_width=True)    
+
+
+elif plot_type == 'All':
+    st.write('You selected All.')
+    all_data = combining_dfs(price_df, news_sentiment_df, alpha_sentiment_df)
+    all_plot = create_all_plot(all_data)
+    st.plotly_chart(all_plot, use_container_width=True)    
+
+
+# Get DataFrame from ML csv
+
+# def get_ml_df(csv_name):
+#   csv_path = Path(f"../data/ML/{}")
+#   csv_data = pd.read_csv(csv_path)
+#   csv_data.set_index(pd.to_datetime(csv_data['Date'], infer_datetime_format=True), inplace=True)
+#   csv_data = csv_data.drop(columns=['Date'])
+#   csv_df = pd.DataFrame(csv_data)
+
+#   return csv_df
+
+
+
+# Radio Button to see ML dataframe and plot
+
+ml_plot_type = st.radio(
+     "Would you like to see a ML model trained and tested on the current Cyrpto Currency?:",
+     ('No', 'Yes'))
+
+if ml_plot_type == 'No':
+    None
+
+elif ml_plot_type =='Yes':
+
+    for w in crypto_list:
+        if option == w:
+            w_path = Path(f'../data/ML/{w}_ml_df.csv')
+            w_data = pd.read_csv(w_path)
+            w_data.set_index(pd.to_datetime(w_data['Date'], infer_datetime_format=True), inplace=True)
+            w_data = w_data.drop(columns=['Date'])
+
+    st.dataframe(w_data, width=1800)
+
+    ml_df = pd.DataFrame(w_data)
+    final_ml_df = ml_df.reset_index()
+
+    fig_ml = px.line(final_ml_df, x="Date", y=["Real","Predicted"], title=f'{option} - Real vs. Predicted Closing Prices')
+    st.plotly_chart(fig_ml, use_container_width=True)
+
+
+
+
 
 
